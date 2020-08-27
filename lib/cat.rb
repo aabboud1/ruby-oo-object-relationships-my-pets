d8642022d7b610b678ece37909862e5de64bb9f4
conrad_buys <conradbuys11@gmail.com>
2020-08-27 15:56:39 -0400

    final changes

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,22 @@
 class Cat
-  # code goes here
+
+  attr_accessor :owner, :mood
+  attr_reader :name
+  @@cats = []
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@cats.push(self)
+    owner.cats.push(self)
+  end
+
+  def self.all
+    @@cats
+  end
+
 end
+
+#conrad = Owner.new(iuyoarfdouigaerg)
+#snuggums = Cat.new("Snuggums",conrad)
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,71 @@
 class Owner
   # code goes here
+  attr_reader :name, :species
+  @@owners = []
+  attr_accessor :cats, :dogs
+
+  def initialize(name)
+    @name = name
+    @species = "human"
+    @@owners.push(self)
+    @cats = []
+    @dogs = []
+  end
+
+  def self.all
+    @@owners
+  end
+
+  def self.count
+    @@owners.count
+  end
+
+  def self.reset_all
+    @@owners.clear
+  end
+
+  def say_species
+    "I am a #{species}."
+  end
+
+  def buy_cat(name)
+    name = Cat.new("#{name}", self)
+  end
+
+  def buy_dog(name)
+    name = Dog.new("#{name}", self)
+  end
+
+  def walk_dogs
+    self.change_pets_mood(self.dogs, "happy")
+  end
+
+  def feed_cats
+    self.change_pets_mood(self.cats, "happy")
+  end
+
+  def change_pets_mood(pets, mood)
+    pets.each do |pet|
+      pet.mood = mood
+    end
+  end
+
+  def reset_pets_owner(pets)
+    pets.each do |pet|
+      pet.owner = nil
+      pets.delete(pet)
+    end
+  end
+
+  def sell_pets
+    self.change_pets_mood(self.cats, "nervous")
+    self.reset_pets_owner(self.cats)
+    self.change_pets_mood(self.dogs, "nervous")
+    self.reset_pets_owner(self.dogs)
+  end
+
+  def list_pets
+    "I have #{self.dogs.length} dog(s), and #{self.cats.length} cat(s)."
+  end
+
 end
